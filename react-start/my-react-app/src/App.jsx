d6dea5510b1195864b9ef0066e96ac4a93fc4c91
Dipesh Dalmia <dipesh.d@idsil.loc>
2026-05-21 11:05:16 +0530

    second commit

--- react-start/my-react-app/src/App.jsx
+++ react-start/my-react-app/src/App.jsx
@@ -9,6 +9,10 @@ function App() {
   const [text,setText]=useState("")
   const [myCar,setMyCar]=useState("volvo")
 
+  const [inputs,setInputs]=useState({})
+  const [inputs1,setInputs1]=useState({})
+  const [selectedFruit,setSelectedFruit]=useState("banana")
+
   function handleChange(event) {
     setName(event.target.value)
   }
@@ -21,6 +25,29 @@ function App() {
   }
   function handleCar(event){
     setMyCar(event.target.value)
+  }
+  function handleInput(event){
+    const name=event.target.name;
+    const value=event.target.value;
+    setInputs(values=>({...values,[name]:value}))
+  }
+  function handleInputs1(event){
+    const value=event.target.type==='checkbox'?event.target.checked:event.target.value;
+    const name=event.target.name;
+    setInputs1(values=>({...values,[name]:value}))
+  }
+  function handleSubmit1(event){
+     event.preventDefault()
+    let fillings=""
+    if(inputs1.tomato)fillings+="tomato "
+    if(inputs1.onion)fillings+="onion "
+    if (fillings == '') fillings = 'no fillings';
+    alert(`${inputs1.yourname} orders a sandwich with ${fillings}`)
+   
+  }
+  function handleSubmit2(event){
+    event.preventDefault()
+    alert(`You selected ${selectedFruit}`)
   }
 
   return (
@@ -43,6 +70,38 @@ function App() {
         <option value="volvo">volvo</option>
         <option value="fiat">fiat</option>
       </select>
+
+      <form>
+        <input type="text" name="firstName" onChange={handleInput} placeholder="Enter your first name" value={inputs.firstname} />
+        <input type="text" name="lastName" onChange={handleInput} placeholder="Enter your last name" value={inputs.lastname}/>
+      </form>
+      <label>First name:{inputs.firstName}</label>
+      <br />
+      <label>Last name:{inputs.lastName}</label>
+
+      <br/><br/>
+      <form onSubmit={handleSubmit1}>
+        <label>Enter your name:</label>
+        <input type="text" name="yourname" value={inputs1.yourname} onChange={handleInputs1} placehlder="Enter your name"/>
+        <label>tomato</label>
+        <input type="checkbox" name="tomato" checked={inputs1.tomato} onChange={handleInputs1}/>
+        <label>onion</label>
+        <input type="checkbox" name="onion" checked={inputs1.onion} onChange={handleInputs1}/>
+        <button type="submit">Submit</button>
+      </form>
+
+      <form onSubmit={handleSubmit2}>
+        <p>select a fruit</p>
+        <label>apple</label>
+        <input type="radio" name="fruit" value="apple" checked={selectedFruit==='apple'} onChange={(e)=>setSelectedFruit(e.target.value)}/>
+        <label>banana</label>
+        <input type="radio" name="fruit" value="banana" checked={selectedFruit==='banana'} onChange={(e)=>setSelectedFruit(e.target.value)}/>
+        <label>orange</label>
+        <input type="radio" name="fruit" value="orange" checked={selectedFruit==='orange'} onChange={(e)=>setSelectedFruit(e.target.value)}/>
+        <p>Selected fruit:{selectedFruit}</p>
+        <button type="submit">Submit</button>
+
+      </form>
     </>
   )
 }
